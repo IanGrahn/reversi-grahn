0edd5c34eef772a25c70099e0310cbcf60a5d869
Ian Grahn <igrahn@westmont.edu>
2023-03-20 19:13:04 -0700

    a

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ io.on('connection', (socket) => {
 
     /** joinroom command handler */
 
-    socket.on('join_room', () => {
+    socket.on('join_room', (payload) => {
         serverLog('Server received a command','\'join_room\'',JSON.stringify(payload));
     });
 });
